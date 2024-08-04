230e9ade85128b82499a353373551eebedcf803c
Dri || ZNƏ <146328983+ZeltNamizake@users.noreply.github.com>
2024-08-04 17:40:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
+const cookieParser = require("cookie-parser");
 const port = 3000;
+
 
 app.use((req, res, next) => {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     console.log('Alamat IP:', ip);
+    cookieParser();
     next();
 });
 
 app.get('/', (req, res) => {
+    const cookies = req.cookies;
     res.send(' ');
 });
 
